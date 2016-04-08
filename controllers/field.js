e6df7c6ddf5bba05aedf810558c54290eed54a0b
Hazem Khaled <hazem.khaled@gmail.com>
2016-04-08 19:54:00 +0200

    Last commit generate another issue in iOS, now iOS return color the row while fix in last commit set for Android only

--- controllers/field.js
+++ controllers/field.js
@@ -137,11 +137,11 @@ function setInput(input) {
 function showError(show) {
 
   if (show) {
-    Alloy.addClass(controllerParam, $.background, 'errorRow');
+    Alloy.addClass(controllerParam, OS_IOS ? $.row : $.background, 'errorRow');
     Alloy.addClass(controllerParam, $.label, 'errorLabel');
 
   } else {
-    Alloy.removeClass(controllerParam, $.background, 'errorRow');
+    Alloy.removeClass(controllerParam, OS_IOS ? $.row : $.background, 'errorRow');
     Alloy.removeClass(controllerParam, $.label, 'errorLabel');
   }
 }
